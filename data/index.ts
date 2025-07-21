b33851b156a2b18432b035e52f1471a61980cfa4
Samandar Sariboyev <samandarsariboyev69@gmail.com>
2025-07-21 06:13:42 +0500

    Update Experience component and workExperience data with Uzbek localization, changing titles and descriptions to reflect local language and context.

--- data/index.ts
+++ data/index.ts
@@ -173,29 +173,29 @@ export const companies = [
 export const workExperience = [
   {
     id: 1,
-    title: "Frontend Engineer Intern",
-    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    title: "Backend dasturchi",
+    desc: "Ideal Study CRM, DigiContract, Examify.uz, LoadMail va boshqa loyihalarda backend dasturlar yozdim.",
     className: "md:col-span-2",
     thumbnail: "/exp1.svg",
   },
   {
     id: 2,
-    title: "Mobile App Dev - JSM Tech",
-    desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+    title: "Mobil dasturchi",
+    desc: "Tez-Taxi, MyTok, VolTex, Evakuator kali mobil dasturlar yozdim",
     className: "md:col-span-2", // change to md:col-span-2
     thumbnail: "/exp2.svg",
   },
   {
     id: 3,
-    title: "Freelance App Dev Project",
-    desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+    title: "Team leader",
+    desc: "Dastur loyihasini ishlab chiqishdan to uni do'konga yuklaguncha jamoaga rahbarlik qildim",
     className: "md:col-span-2", // change to md:col-span-2
     thumbnail: "/exp3.svg",
   },
   {
     id: 4,
-    title: "Lead Frontend Developer",
-    desc: "Developed and maintained user-facing features using modern frontend technologies.",
+    title: "Cyber security",
+    desc: "Kiberxavfsizlik sohasi bo'yicha tizimlarni pentest qilaman",
     className: "md:col-span-2",
     thumbnail: "/exp4.svg",
   },
